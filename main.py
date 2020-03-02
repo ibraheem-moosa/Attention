2a753b38190c7127a5535804af46a962bd8a12cd
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2020-03-02 23:07:00 +0600

    Dataset splitting and CrossEntropyLoss for LM.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
-from torch.utils.data import Dataset
+from torch.optim import SGD
+from torch.utils.data import Dataset, Subset, DataLoader
+
+import random
 
 from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
 from ignite.metrics import Accuracy, Loss
@@ -35,7 +38,8 @@ class Text8DataSet(Dataset):
         with open(path) as f:
             text = f.read()
         chars = sorted(list(set(text)))
-        assert(len(chars) < 255)
+        self.vocab_size = len(chars)
+        assert(self.vocab_size < 255)
         index_to_char = dict((i, c) for (i, c) in enumerate(chars))
         char_to_index = dict((c, i) for (i, c) in enumerate(chars))
         self.index_to_char = index_to_char
@@ -48,14 +52,57 @@ class Text8DataSet(Dataset):
     def __getitem__(self, idx):
         x = self.text[idx * self.seq_len: (idx + 1) * self.seq_len]
         y = self.text[idx * self.seq_len + 1: (idx + 1) * self.seq_len + 1]
-        return x, y
+        return x.to(torch.long), y.to(torch.long)
 
     def __len__(self):
         return self.length
 
+class CrossEntropyLanguageModel(nn.Module):
+    def __init__(self):
+        super(CrossEntropyLanguageModel, self).__init__()
+        self.ce = nn.CrossEntropyLoss()
+
+    def forward(self, inp, targ):
+        inp = inp.view((-1, inp.shape[-1]))
+        targ = targ.view((-1,))
+        return self.ce(inp, targ)
 
 if __name__ == '__main__':
-    model = SimpleRNNLanguageModel(27, 8, 1)
-    print(model(torch.zeros((10, 8), dtype=torch.long)).shape)
     ds = Text8DataSet('./text8', 10)
     print(len(ds))
+    ds_len = len(ds)
+    indices = list(range(ds_len))
+    random.seed(42)
+    random.shuffle(indices)
+    te_ratio = 0.01
+    te_ds_len = int(ds_len * te_ratio)
+    va_ratio = 0.04
+    va_ds_len = int(ds_len * va_ratio)
+    tr_indices = indices[:-va_ds_len-te_ds_len]
+    va_indices = indices[-va_ds_len-te_ds_len:-te_ds_len]
+    te_indices = indices[-te_ds_len:]
+    tr_ds, va_ds, te_ds = Subset(ds, tr_indices), Subset(ds, va_indices), Subset(ds, te_indices)
+    tr_dl = DataLoader(tr_ds, batch_size=2, shuffle=True, drop_last=True)
+    va_dl = DataLoader(va_ds, batch_size=2)
+
+    hidden_size = 8
+    num_layers = 1
+    model = SimpleRNNLanguageModel(ds.vocab_size, hidden_size, num_layers)
+    optimizer = SGD(model.parameters(), lr=1e-3, nesterov=True, momentum=0.9)
+    loss = CrossEntropyLanguageModel()
+    x, y = next(iter(tr_dl))
+    out = model(x)
+    print(loss(out, y))
+
+    trainer = create_supervised_trainer(model, optimizer, loss)
+    evaluator = create_supervised_evaluator(model, metrics={'acc': Accuracy(), 'ce': Loss(loss)})
+
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def log_tr_loss(trainer):
+        print('Epoch {}: Loss: {:.6f}'.format(trainer.state.epoch, trainer.state.output))
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_va_loss(trainer):
+        evaluator.run(va_dl)
+        metrics = evaluator.state.metrics
+        print('Epoch {}: Va Acc: {:.6f} Va Loss: {:.6f}'.format(trainer.state.epoch, metrics['acc'], metrics['ce'])) 
