5f45b873e464967948100d90bb832837b1630eef
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2020-03-06 21:28:49 +0600

    Clip grad to 1.0

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch.nn.utils import clip_grad_norm_
 from torch.optim import SGD, Adam
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import Dataset, Subset, DataLoader
@@ -9,7 +10,7 @@ import random
 import datetime
 from collections import Counter, defaultdict
 
-from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
+from ignite.engine import Events, Engine, create_supervised_trainer, create_supervised_evaluator
 from ignite.metrics import Accuracy, Loss
 
 import text8dataset
@@ -28,7 +29,7 @@ class CrossEntropyLanguageModel(nn.Module):
 
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    ds = text8dataset.Text8WordDataSet('./text8', seq_len=20, max_vocab_size=10000)
+    ds = text8dataset.Text8WordDataSet('./text8', seq_len=20)
     ds_len = len(ds)
     print(ds_len, ds.vocab_size)
     indices = list(range(ds_len))
@@ -48,19 +49,31 @@ if __name__ == '__main__':
     te_dl = DataLoader(te_ds, batch_size=bs)
     print(len(tr_dl))
 
-    emb_size = 128
     hidden_size = 128
+    emb_size = 128
     num_layers = 1
-    model = lmmodels.SimpleRNNLanguageModel(ds.vocab_size, emb_size, hidden_size, num_layers)
-    optimizer = Adam(model.parameters())
-    loss = CrossEntropyLanguageModel()
+    model = lmmodels.SimpleRNNLanguageModel(ds.vocab_size, emb_size, hidden_size, num_layers).to(device)
+    optimizer = Adam(model.parameters(), lr=1e-3)
+    criterion = CrossEntropyLanguageModel()
     scheduler = ReduceLROnPlateau(optimizer, patience=2, verbose=True)
 
-    trainer = create_supervised_trainer(model, optimizer, loss, device=device)
+    def update_model(trainer, batch):
+        model.train()
+        optimizer.zero_grad()
+        x, y = batch
+        x, y = x.to(device), y.to(device)
+        y_pred = model(x)
+        loss = criterion(y_pred, y)
+        loss.backward()
+        clip_grad_norm_(model.parameters(), 1.0)
+        optimizer.step()
+        return loss.item()
+
+    trainer = Engine(update_model)
     metrics = {
             'acc': Accuracy(
                 output_transform=lambda y_pred: (y_pred[0].view((-1, ds.vocab_size)), y_pred[1].view((-1,)))),
-            'ce': Loss(loss)}
+            'ce': Loss(criterion)}
     evaluator = create_supervised_evaluator(model, metrics=metrics, device=device)
 
     @trainer.on(Events.ITERATION_COMPLETED(every=500))
