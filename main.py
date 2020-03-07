4b54fa0d49f11af9935f4a7828f382bb3599d1c3
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2020-03-07 23:34:56 +0600

    Set lr to 2e-2

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
     # lr_finder.run(tr_dl, epoch_length=lr_finder_steps)
 
     epochs = 25
-    scheduler = OneCycleLR(optimizer, max_lr=5e-2, epochs=epochs, steps_per_epoch=len(tr_dl), pct_start=0.5, anneal_strategy='linear')
+    scheduler = OneCycleLR(optimizer, max_lr=2e-2, epochs=epochs, steps_per_epoch=len(tr_dl), pct_start=0.5, anneal_strategy='linear')
     trainer = Engine(update_model)
     metrics = {
             'acc': Accuracy(
