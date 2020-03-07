e156d529122aa63961a8fd1c330e666a81116e52
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2020-03-07 11:59:03 +0600

    Setting maxlr for 1cycle to 2e-1

--- main.py
+++ main.py
@@ -93,9 +93,9 @@ if __name__ == '__main__':
         plt.show()
         sys.exit()
 
-    lr_finder.run(tr_dl, epoch_length=lr_finder_steps)
+    # lr_finder.run(tr_dl, epoch_length=lr_finder_steps)
 
-    scheduler = OneCycleLR(optimizer, max_lr=1e1, epochs=25, steps_per_epoch=len(tr_dl), pct_start=0.5, anneal_strategy='linear')
+    scheduler = OneCycleLR(optimizer, max_lr=2e-1, epochs=25, steps_per_epoch=len(tr_dl), pct_start=0.5, anneal_strategy='linear')
     trainer = Engine(update_model)
     metrics = {
             'acc': Accuracy(
