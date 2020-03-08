fd6674a729b99b1b47396c91c4ba24c17bded557
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2020-03-08 12:26:07 +0600

    Set intial div factor to 100

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
     # lr_finder.run(tr_dl, epoch_length=lr_finder_steps)
 
     epochs = 25
-    scheduler = OneCycleLR(optimizer, max_lr=0.1, epochs=epochs, steps_per_epoch=len(tr_dl), pct_start=0.5, anneal_strategy='linear')
+    scheduler = OneCycleLR(optimizer, max_lr=0.1, epochs=epochs, steps_per_epoch=len(tr_dl), pct_start=0.5, anneal_strategy='linear', div_factor=100)
     trainer = Engine(update_model)
     metrics = {
             'acc': Accuracy(
