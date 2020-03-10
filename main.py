f09e80042d1b05460cafbd320d00f55bd6e959a0
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2020-03-10 11:51:22 +0600

    Enable optimizer weight loading

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
     @trainer.on(Events.STARTED)
     def load_model_weights(trainer):
         model.load_state_dict(torch.load(checkpoint_dir + '/model.pth'))
-        # optimizer.load_state_dict(torch.load(checkpoint_dir + '/optimizer.pth'))
+        optimizer.load_state_dict(torch.load(checkpoint_dir + '/optimizer.pth'))
     @trainer.on(Events.STARTED)
     def reset_lr(trainer):
         for param_group in optimizer.param_groups:
