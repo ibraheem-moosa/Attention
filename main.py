0c9a9aa20326d6a2b6ab317855623017b23a2ce0
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2020-03-12 17:21:44 +0600

    Set lr to 1e-3, disable weight loading

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
     hidden_size = 1024
     emb_size = 16
     num_layers = 4
-    lr = 1e-6
+    lr = 1e-3
     model = lmmodels.GRUSharedEmbeddingLanguageModel(tr_ds.vocab_size, emb_size, hidden_size, num_layers).to(device)
     optimizer = Adam(model.parameters(), lr=lr)
     criterion = CrossEntropyLanguageModel()
@@ -110,6 +110,7 @@ if __name__ == '__main__':
     def scheduler_step(trainer):
         scheduler.step()
     """
+    """
     @trainer.on(Events.STARTED)
     def load_model_weights(trainer):
         try:
@@ -121,6 +122,7 @@ if __name__ == '__main__':
     def reset_lr(trainer):
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr
+    """
     @trainer.on(Events.ITERATION_COMPLETED(every=16))
     def log_tr_loss(trainer):
         print(datetime.datetime.now())
